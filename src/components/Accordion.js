772dd961c676953c2c395d6184165d7a05b81475
Alina Revutska <alinkarevutska@gmail.com>
2023-03-22 17:22:33 +0000

    created event handler outside the mapping function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,14 +2,17 @@ import { useState } from "react";
 
 function Accordion({items}) {
     const [expandedIndex, setExpandedIndex] = useState(0);
+    const handleClick = (index) => {
+        setExpandedIndex(index)
+    };
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
         return (
-            <div key={item.id}>
+            <div key={item.id} onClick={() => handleClick(index)}>
                 <div>{item.label}</div>
-               {isExpanded && <div>{item.content}</div>}
+               {isExpanded && <div>{item.content}</div>} {/* if false, div is not rendered*/}
             </div>
         )
     })
